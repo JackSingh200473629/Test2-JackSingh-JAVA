9befb1356c84badbb2ff7bb158427dd832899e02
Jack <jack@whiteshark.ca>
2023-08-14 10:28:27 -0400

    Question 2 Completed

--- src/main/java/com/georgiancollege/test2/Address.java
+++ src/main/java/com/georgiancollege/test2/Address.java
@@ -1,4 +1,24 @@
 package com.georgiancollege.test2;
 
 public class Address {
+    private String address;
+    private String city;
+    private String postalCode;
+    private String state;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getState() {
+        return state;
+    }
 }
