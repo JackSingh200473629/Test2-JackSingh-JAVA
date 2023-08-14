ec5201c6a74d97461cf65a5556444fae28d5708d
Jack <jack@whiteshark.ca>
2023-08-14 10:24:31 -0400

    Question 1 Completed

--- src/main/java/com/georgiancollege/test2/User.java
+++ src/main/java/com/georgiancollege/test2/User.java
@@ -1,4 +1,57 @@
 package com.georgiancollege.test2;
 
+import com.google.gson.annotations.SerializedName;
+
 public class User {
+    private int id;
+    private String FirstName;
+    private String LastName;
+    private int age;
+    private String email;
+    @SerializedName("pHoNe")
+    private String phone;
+    private String birthDate;
+    private String university;
+    private String image;
+    private Address address;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.georgiancollege.test2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
 
     opens com.georgiancollege.test2 to javafx.fxml;
     exports com.georgiancollege.test2;
